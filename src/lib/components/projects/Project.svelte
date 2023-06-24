cc17171117d3e12e10bac9d5a8ea704325cc6c02
Safwan Rahman <Safwanrahman9@gmail.com>
2023-06-24 14:50:38 -0600

    Edited the view projects page

--- src/lib/components/projects/Project.svelte
+++ src/lib/components/projects/Project.svelte
@@ -2,9 +2,9 @@
     import { onMount } from "svelte";
     import { walletAddress } from "$lib/stores/provider";
     import {  
-      projects,
-      
+      projects
     } from '$lib/config'
+    import { categoryCreatorExplore } from "$lib/stores/state";
 
 
 
@@ -12,41 +12,69 @@
 
 
 
-
 <div class="">
   <div class="mx-auto max-w-2xl px-6 py-18 sm:px-6 sm:py-24 lg:max-w-[80rem] lg:px-8 font-mono">   
     <div class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
-      {#each projects as project}
-        <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white hover:scale-[1.02] transition ">
-          <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-95 sm:h-96">
-            <img src="{project.images[0]}" alt="artist" class="h-full w-full object-cover object-center sm:h-full sm:w-full scale-[0.5]">
+      {#each projects  as project}
+        {#if project.category == $categoryCreatorExplore || $categoryCreatorExplore == 'All'}
+          <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white hover:scale-[1.02] transition ">
+            <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-95 sm:h-96">
+              <img src="{project.images[0]}" alt="artist" class="h-full w-full object-cover object-center sm:h-full sm:w-full scale-[0.5]">
+            </div>
+            <div class="flex flex-1 flex-col space-y-1 p-4">
+              <h3 class="text-md font-bold">{project.name}</h3>
+              <span aria-hidden="true" class="text-xs text-gray-600">{project.profession}</span>
+
+
+              <div id='location' class=''>
+                <span class="text-sm text-gray-500 mt-3">
+                  <span class=''> 
+                    {project.country}
+                  </span>{project.location}
+                </span>
+              </div>
+
+
+
+
+              
+              <div class=''>
+                {#if project.goal > 0}
+                  <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                    <div class="bg-[#63C132] h-2.5 rounded-full" style="width: 45%"></div>
+                  </div>
+                {:else}
+                  <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                    <div class="bg-[#63C132] h-2.5 rounded-full" style="width: 100%"></div>
+                  </div>
+                {/if}
+              </div>
+              <div class='pt-2'>
+                <span class='text-2xl'>
+                  {project.totalValue}Ξ raised
+                </span>
+
+              </div>  
+              <div class="flex flex-col">
+                  <span class="text-sm italic  text-gray-400 mt-2">{project.supporters} supporters this month</span>
+              
+                  {#if Number(project.minimum) > 0}
+                  <span class='text-sm italic  text-gray-400 mt'>
+                    {project.minimum} ETH minimum pledge to participate
+                  </span>
+
+                {/if}
+                </div>
+              <div class='pt-5'>
+
+                <button class='px-2 py-2 bg-[] border-[#EBEBEB] border rounded-xl shadow-sm hover:scale-[1.06] transition hover:shadow-md'>View</button>
+                <button on:click={gotoPledge} type="button" class="ml-3 inline-flex items-center rounded-md bg-[#9649cb] px-3 py-2 text-sm font-semibold text-white shadow-sm
+                hover:bg-[#8836BF] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:scale-[1.05] transition">Pledge</button>
+              </div>
+
+            </div>
           </div>
-          <div class="flex flex-1 flex-col space-y-1 p-4">
-            <h3 class="text-md font-bold">{project.name}</h3>
-            <span aria-hidden="true" class="text-xs text-gray-600">{project.category}</span>
-
-
-            <div id='location' class=''>
-              <span class="text-sm text-gray-500 mt-3">
-                <span class=''> 
-                  {project.country}
-                </span>{project.location}
-              </span>
-            </div>
-
-
-            <div class="flex flex-row">
-                <span class="text-sm italic  text-gray-600 basis-3/4 mt-2">{project.subscribers.length != undefined ? project.subscribers.length : '0'} subscribers this month</span>
-            </div>
-
-            <div class='pt-5'>
-
-              <button class='px-2 py-2 bg-[] border-[#EBEBEB] border rounded-xl shadow-sm hover:scale-[1.06] transition'>View</button>
-              <button class='px-2 py-2 bg-[#ee4266] border-[#ee4266] text-white border rounded-xl shadow-sm hover:scale-[1.06] transition'>Support</button>
-            </div>
-
-          </div>
-        </div>
+        {/if}
       {/each}
     </div>
   </div>
--- src/lib/stores/state.ts
+++ src/lib/stores/state.ts
@@ -1,10 +1,13 @@
 export const ssr = false;
 import { BigNumber } from "ethers";
 import { writable } from "svelte/store";
+import type { Project } from "../config";
 
 
 import '$lib/globalState';
 
 export const balanceOnBlock = writable<BigNumber>(BigNumber.from(0)); 
+export const selectedFundr = writable<Project>()
 
-export const categoryCreatorExplore = writable<string>('')
+
+export const categoryCreatorExplore = writable<string>('All')
