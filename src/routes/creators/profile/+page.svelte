3b797b095c062b7011f5dbd71ef36e3a330abf40
Safwan Rahman <Safwanrahman9@gmail.com>
2023-06-24 11:02:53 -0600

    started the profile page

---
+++ src/routes/creators/profile/+page.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    
+
+	import Profile from '$lib/components/projects/profile/Profile.svelte';
+	import Navbar from '$lib/components/reusable/Navbar.svelte';
+	
+</script>
+
+
+<Navbar/>
+<main class="flex flex-col p-10 w-screen h-screen items-center gap-5 font-mono">
+	<Profile/>
+</main>
+
+
+
