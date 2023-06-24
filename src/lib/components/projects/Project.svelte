cc8d79afe6a043d79e9cb53dcdb5f0041c34a268
nickmura <nickmuradev@gmail.com>
2023-06-24 13:19:26 -0600

    Edited Project.svelte card a lot more

--- src/lib/components/projects/Project.svelte
+++ src/lib/components/projects/Project.svelte
@@ -1,67 +1,53 @@
 <script lang='ts'>
     import { onMount } from "svelte";
     import { walletAddress } from "$lib/stores/provider";
+    import {  
+      projects,
+      
+    } from '$lib/config'
 
 
-    interface Value {
-      symbol: string
-      amount: number
-    }
-    interface Subscriber {
-      address: string
-      chainid: string
-      contribution: Value[]
-    }
-    interface Project {
-      name: string,
-      timeStarted: number // block time stamps, or a ContractBlockStamps[] for multiple chains
-      signers: string[]
-      totalValue: number
-      subscribers: Subscriber[]
 
-    }
 </script>
 
 
 
 
-<div class="rounded-2.5xl max-w-md p-6 ring-2 s xl:p-10 ring-gray-200">
-    <h3 id="tier-freelancer" class="text-lg font-semibold leading-8 text-gray-900">Freelancer</h3>
-    <p class="mt-4 text-sm leading-6 text-gray-600">The essentials to provide your best work for clients.</p>
-    <p class="mt-6 flex items-baseline gap-x-1">
-      <!-- Price, update based on frequency toggle state -->
-      <span class="text-4xl font-bold tracking-tight text-gray-900">$15</span>
-      <!-- Payment frequency, update based on frequency toggle state -->
-      <span class="text-sm font-semibold leading-6 text-gray-600">/month</span>
-    </p>
-    <a href="/" aria-describedby="tier-freelancer" class="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-indigo-600 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-indigo-600">Buy plan</a>
-    <ul class="mt-8 space-y-3 text-sm leading-6 xl:mt-10 text-gray-600">
-      <li class="flex gap-x-3">
-        <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-        </svg>
-        5 products
-      </li>
-      <li class="flex gap-x-3">
-        <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-        </svg>
-        Up to 1,000 subscribers
-      </li>
-      <li class="flex gap-x-3">
-        <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-        </svg>
-        Basic analytics
-      </li>
-      <li class="flex gap-x-3">
-        <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-        </svg>
-        48-hour support response time
-      </li>
-    </ul>
+<div class="">
+  <div class="mx-auto max-w-2xl px-6 py-18 sm:px-6 sm:py-24 lg:max-w-[80rem] lg:px-8 font-mono">   
+    <div class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+      {#each projects as project}
+        <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white hover:scale-[1.02] transition ">
+          <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-95 sm:h-96">
+            <img src="{project.images[0]}" alt="artist" class="h-full w-full object-cover object-center sm:h-full sm:w-full scale-[0.5]">
+          </div>
+          <div class="flex flex-1 flex-col space-y-1 p-4">
+            <h3 class="text-md font-bold">{project.name}</h3>
+            <span aria-hidden="true" class="text-xs text-gray-600">{project.category}</span>
+
+
+            <div id='location' class=''>
+              <span class="text-sm text-gray-500 mt-3">
+                <span class=''> 
+                  {project.country}
+                </span>{project.location}
+              </span>
+            </div>
+
+
+            <div class="flex flex-row">
+                <span class="text-sm italic  text-gray-600 basis-3/4 mt-2">{project.subscribers.length} subscribers this month</span>
+            </div>
+
+            <div class='pt-5'>
+
+              <button class='px-2 py-2 bg-[] border-[#EBEBEB] border rounded-xl shadow-sm hover:scale-[1.06] transition'>View</button>
+              <button class='px-2 py-2 bg-[#ee4266] border-[#ee4266] text-white border rounded-xl shadow-sm hover:scale-[1.06] transition'>Support</button>
+            </div>
+
+          </div>
+        </div>
+      {/each}
+    </div>
   </div>
-
-
-
+</div>
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -7,6 +7,46 @@ export type Network = {
   blockExplorerUrl?: string
 }
 
+type ExploreNavbar = 'Events' | 'Creators' | 'Community' | 'Orgs'
+
+
+export interface Value {
+  contract: string | null
+  txid: string
+  amount: number
+  
+}
+export interface Subscriber {
+  address: string,
+  chainid: string,
+  value: Value[] | null
+}
+export interface Project {
+  name: string,
+  category: ExploreNavbar
+  timeStarted: number // block time stamps, or a ContractBlockStamps[] for multiple chains
+  signers: string[]
+  totalValue: number
+  subscribers: Subscriber[]
+
+}
+
+
+export const projects = [
+  {
+    name: 'ETHParis Poker Tournament',
+    country: 'France',
+    location: 'Paris',
+    images: ['https://pbs.twimg.com/profile_images/1524436306205589510/CssJu0MT_400x400.jpg'],
+    category: 'Events',
+    signers: [],
+    totalValue: 1558,
+    subscribers: [{address: '0xA73F698846A43c6a87f7Aba3506eE7783f3AfeC2', chainid: 0xaa36a7, value: { contract: null, amount: 50000000000, txid: null }}],
+    
+
+  }
+]
+
 export const NETWORKS: Network[] = [
   {
     chainId: 1,
