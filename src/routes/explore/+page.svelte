6a6ceff3f4a91a9ffa28689b7af583ccea3f8c4d
nickmura <nickmuradev@gmail.com>
2023-06-24 08:05:19 -0600

    added minimum auth provider for metamask component

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,10 +1,16 @@
 <script lang='ts'>
     import Project from "$lib/components/projects/Project.svelte";
+    import Navbar from "$lib/components/reusable/Navbar.svelte";
 
     
 </script>
 
-<main>
+
+<Navbar/>
+
+
+<main >
+    
     <Project/>
 </main>
 
