436500c354db9a47e5c466e0d8ea70efc42dc6b1
0xjimmy <me@0xjimmy.xyz>
2023-06-25 20:08:26 +1200

    Nav bar on layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	
 	import '../app.css';
+	import Navbar from '$lib/components/reusable/Navbar.svelte';
 </script>
 
+<Navbar />
 <slot />
--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,15 +1,7 @@
-<script lang='ts'>
-
-	import Navbar from '$lib/components/reusable/Navbar.svelte';
+<script lang="ts">
 	import Create from '$lib/components/projects/create/Create.svelte';
-	
 </script>
 
-
-<Navbar/>
-
 <main class="flex flex-col p-10 w-screen h-screen items-center gap-5 font-mono">
-    <Create/>
+	<Create />
 </main>
-
-
